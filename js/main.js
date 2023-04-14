2eec99ecd5cd418b2e67e130a6518aa931647b5f
Matthew Miner <mminer237@gmail.com>
2023-04-14 11:03:03 -0500

    Add license to JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
+
 "use strict";
 
 const inputLink = document.getElementById("input-link");
@@ -356,3 +358,5 @@ function drawChart(numbers, mean, median, mode) {
 		}
 	});
 }
+
+// @license-end
